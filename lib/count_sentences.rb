d9acb501704e7b18b561ef519afa5ea17a711b61
Amy Fogelman <abfnj1@gmail.com>
2019-07-14 11:16:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -20,7 +20,7 @@ end
 
   def count_sentences
    sentence_array = self.split(/[.?!]/)
-    sentence_array.delete_if{|sentence| sentence.empty?}
-    sentence_array.length
+   sentence_array.delete_if{|sentence| sentence.empty?}
+   sentence_array.length
   end
 end
